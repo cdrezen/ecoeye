e14be65e35142eba735b32989f4a75c35ee4c70e
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2025-05-13 18:09:49 +0200

    take picture & init refactoring

--- src/main.py
+++ src/main.py
@@ -26,9 +26,23 @@ vdiv_bat = vdiv_build()
 solartime = suntime(operation_time,sunrise_hour,sunrise_minute,sunset_hour,sunset_minute)
 rtc = rtc()
 exposure_values = exposure_bracketing_values if use_exposure_bracketing else [1]
+last_exposure = 0
 illumination = Illumination()
 current_folder, picture_count, detection_count = None, 0, 0
-
+is_night = not solartime.is_daytime()
+# future image width and height
+image_width = windowing_w if use_sensor_windowing else sensor.width()
+image_height = windowing_h if use_sensor_windowing else sensor.height()
+#start counting time
+start_time_status_ms = 0
+start_time_blending_ms = 0
+start_time_active_LED_ms = 0
+#set trigger and detected state
+triggered = False
+detected = False
+#start clock
+clock = None
+img_ref_fb, img_ori_fb = None, None
 
 def check_battery_sleep(vbat=None, print_status=""):
     # check voltage and save status, if battery too low -> sleep until sunrise
@@ -41,115 +55,131 @@ def check_battery_sleep(vbat=None, print_status=""):
         write_status(vbat,"Battery low - Sleeping", current_folder)
         indicator_dsleep(solartime.time_until_sunrise() + AFTER_SUNRISE_DELAY, active_LED_interval_ms)
 
+def take_picture(do_expose, exposure_mult = None):
+    global last_exposure
+    last_exposure = sensor.get_exposure_us()
 
-# perform quick start from sleep check
-start_check()
+    if (exposure_mult != None):#use_exposure_bracketing
+        #fix the gain so image is stable
+        sensor.set_auto_gain(False, gain_db=sensor.get_gain_db())
+        print("Exposure bracketing bias:",exposure_mult)
+        sensor.set_auto_exposure(False, exposure_us=int(last_exposure*exposure_mult))
+        #wait for new exposure time to be applied
+        sensor.skip_frames(time = 2000)
 
-# On wakeup from deep sleep, fetch variables from files
-if (machine.reset_cause() == machine.DEEPSLEEP_RESET):
-    current_folder, picture_count, detection_count = read_filevars()
-    check_battery_sleep(print_status="Script start - Waking")
+    # at night, turn ON selected illumination LEDs if not always OFF mode
+    if(illumination.can_turn_on(is_night)):
+        illumination.on(message="to take the picture")
 
-# create and initialize new folders only on powerup or soft reset
-if (machine.reset_cause() != machine.DEEPSLEEP_RESET and MODE != 0):
-    # create necessary files & folders
-    current_folder = init_files(current_folder, rtc)
-    picture_count = 0
-    detection_count = 0
-    check_battery_sleep(print_status="Script start - Initialising")
+    if (do_expose):
+        expose(exposure_mode,exposure_bias_day,exposure_bias_night,gain_bias,exposure_ms,gain_dB,is_night)
+    
+    img = sensor.snapshot()
 
-#import mobilenet model and labels
-if(classify_mode != "none"):
-    labels, non_target_indices = load_model()
+    # after picture, turn OFF selected illumination LEDs if not always ON mode
+    if (illumination.can_turn_off()): 
+        illumination.off(message="to save power...")
 
-# verify that wifi shield is connected when wifi is enabled
-if(wifi_enable):
-    wifi_enable = wifishield_isconnnected()
-
-if (frame_differencing_enabled and exposure_mode=="auto"): 
-    print("ATTENTION: using automatic exposure with frame differencing can result in spurious triggers!")
-
-
-# ━━━━━━━━━━ 𝗦𝗘𝗡𝗦𝗢𝗥 𝗜𝗡𝗜𝗧 ━━━━━━━━━━
-#check night time for continuous monitoring
-is_night = not solartime.is_daytime()
-# get future image width and height
-image_width = windowing_w if use_sensor_windowing else sensor.width()
-image_height = windowing_h if use_sensor_windowing else sensor.height()
-
-#indicate initialisation with LED
-LED_WHITE_BLINK(200,3)
-# Reset and initialize the sensor
-sensor.reset()
-#we need RGB565 for frame differencing and MobileNet
-sensor.set_pixformat(sensor_pixformat)
-sensor.set_framesize(sensor_framesize)
-
-if (use_sensor_windowing):
-    sensor.set_windowing(windowing_x,windowing_y,windowing_w,windowing_h)
-
-if (use_sensor_framebuffers): 
-    sensor.set_framebuffers(nb_sensor_framebuffers)
-
-# Give the camera sensor time to adjust
-sensor.skip_frames(time=1000)
-
-#parameter validity checks
-if (windowing_y + windowing_h > sensor.height() or windowing_x + windowing_w > sensor.width()):
-    print("windowing_y:", windowing_y, "windowing_h:", windowing_h, "sensor.height:", sensor.height())
-    print("windowing_x:", windowing_x, "windowing_w:", windowing_w, "sensor.width:", sensor.width())
-    sys.exit("Windowing dim exceeds image dim!")
-
-if(not use_roi and MODE != 0):
-    #assign roi to entire image if we do not use them
-    rois_rects = [(0,0,sensor.width(),sensor.height())]
-    # create directory for jpegs if not already created
-    roi_dirname = '_'.join(map(str,rois_rects[0]))
-    if not roi_dirname in os.listdir(str(current_folder)+"/jpegs"): 
-        os.mkdir(str(current_folder)+"/jpegs"+"/"+roi_dirname)
-        print("Created",roi_dirname,"subfolder")
-
-#adjusting exposure
-# at night, turn ON selected illumination LEDs if not always OFF mode
-if(illumination.can_turn_on(is_night)):
-    illumination.on(message="for exposure adjustment")
-
-expose(exposure_mode,exposure_bias_day,exposure_bias_night,gain_bias,exposure_ms,gain_dB,is_night)
-
-#Frame buffer memory management
-if(frame_differencing_enabled):
-    #de-allocate frame buffer just in case
-    sensor.dealloc_extra_fb()
-    sensor.dealloc_extra_fb()
-    # Take from the main frame buffer's RAM to allocate a second frame buffer.
-    img_ref_fb = sensor.alloc_extra_fb(image_width, image_height, sensor_pixformat)
-    img_ori_fb = sensor.alloc_extra_fb(image_width, image_height, sensor_pixformat)
-
-    print("Saving background image...")
-    picture_count += 1
-    img_ref_fb.replace(sensor.snapshot())
+    #log time
     picture_time = "-".join(map(str,time.localtime()[0:6]))
-    img_ref_fb.save(str(current_folder)+"/jpegs/reference/"+str(picture_count)+"_reference.jpg",quality=jpeg_quality)
-    with open(str(current_folder)+'/images.csv', 'a') as imagelog:
-                imagelog.write(str(picture_count) + ',' + str(picture_time) + ',' + str(sensor.get_exposure_us()) +
-                  ',' + str(sensor.get_gain_db()) + ',' + "NA" + ',' + "reference" + '\n')
-    print("Saved background image - now frame differencing!")
 
-# after exposure adjustment, turn OFF selected illumination LEDs if not always ON mode
-if (illumination.can_turn_off()):
-    illumination.off(message="to save power...")
+    return img, picture_time
 
-#start counting time
-start_time_status_ms = pyb.millis()
-start_time_blending_ms = pyb.millis()
-start_time_active_LED_ms = pyb.millis()
-#set trigger and detected state
-triggered = False
-detected = False
-#start clock
-clock = time.clock()
+def init():
+    global current_folder, picture_count, detection_count, start_time_status_ms, start_time_blending_ms, start_time_active_LED_ms, clock
+    global rois_rects, labels, non_target_indices, wifi_enable, img_ref_fb, img_ori_fb
+    # perform quick start from sleep check
+    start_check()
 
-# ━━━━━━━━━━ 𝗠𝗔𝗜𝗡 𝗟𝗢𝗢𝗣 ━━━━━━━━━━
+    # On wakeup from deep sleep, fetch variables from files
+    if (machine.reset_cause() == machine.DEEPSLEEP_RESET):
+        current_folder, picture_count, detection_count = read_filevars()
+        check_battery_sleep(print_status="Script start - Waking")
+
+    # create and initialize new folders only on powerup or soft reset
+    if (machine.reset_cause() != machine.DEEPSLEEP_RESET and MODE != 0):
+        # create necessary files & folders
+        current_folder = init_files(current_folder, rtc)
+        picture_count = 0
+        detection_count = 0
+        check_battery_sleep(print_status="Script start - Initialising")
+
+    #import mobilenet model and labels
+    if(classify_mode != "none"):
+        labels, non_target_indices = load_model()
+
+    # verify that wifi shield is connected when wifi is enabled
+    if(wifi_enable):
+        wifi_enable = wifishield_isconnnected()
+
+    if (frame_differencing_enabled and exposure_mode=="auto"): 
+        print("ATTENTION: using automatic exposure with frame differencing can result in spurious triggers!")
+
+    ### SENSOR INIT ###
+
+    #indicate initialisation with LED
+    LED_WHITE_BLINK(200,3)
+    # Reset and initialize the sensor
+    sensor.reset()
+    #we need RGB565 for frame differencing and MobileNet
+    sensor.set_pixformat(sensor_pixformat)
+    sensor.set_framesize(sensor_framesize)
+
+    if (use_sensor_windowing):
+        sensor.set_windowing(windowing_x,windowing_y,windowing_w,windowing_h)
+
+    if (use_sensor_framebuffers): 
+        sensor.set_framebuffers(nb_sensor_framebuffers)
+
+    # Give the camera sensor time to adjust
+    sensor.skip_frames(time=1000)
+
+    #parameter validity checks
+    if (windowing_y + windowing_h > sensor.height() or windowing_x + windowing_w > sensor.width()):
+        print("windowing_y:", windowing_y, "windowing_h:", windowing_h, "sensor.height:", sensor.height())
+        print("windowing_x:", windowing_x, "windowing_w:", windowing_w, "sensor.width:", sensor.width())
+        sys.exit("Windowing dim exceeds image dim!")
+
+    if(not use_roi and MODE != 0):
+        #assign roi to entire image if we do not use them
+        rois_rects = [(0,0,sensor.width(),sensor.height())]
+        # create directory for jpegs if not already created
+        roi_dirname = '_'.join(map(str,rois_rects[0]))
+        if not roi_dirname in os.listdir(str(current_folder)+"/jpegs"): 
+            os.mkdir(str(current_folder)+"/jpegs"+"/"+roi_dirname)
+            print("Created",roi_dirname,"subfolder")
+
+    #Frame buffer memory management
+    if(frame_differencing_enabled):
+        #de-allocate frame buffer just in case
+        sensor.dealloc_extra_fb()
+        sensor.dealloc_extra_fb()
+        # Take from the main frame buffer's RAM to allocate a second frame buffer.
+        img_ref_fb = sensor.alloc_extra_fb(image_width, image_height, sensor_pixformat)
+        img_ori_fb = sensor.alloc_extra_fb(image_width, image_height, sensor_pixformat)
+
+        print("Saving background image...")
+        picture_count += 1
+
+        img, picture_time = take_picture(do_expose=True)
+        img_ref_fb.replace(img)
+
+        img_ref_fb.save(str(current_folder)+"/jpegs/reference/"+str(picture_count)+"_reference.jpg",quality=jpeg_quality)
+        with open(str(current_folder)+'/images.csv', 'a') as imagelog:
+                    imagelog.write(str(picture_count) + ',' + str(picture_time) + ',' + str(sensor.get_exposure_us()) +
+                    ',' + str(sensor.get_gain_db()) + ',' + "NA" + ',' + "reference" + '\n')
+        print("Saved background image - now frame differencing!")
+
+    #start counting time
+    start_time_status_ms = pyb.millis()
+    start_time_blending_ms = pyb.millis()
+    start_time_active_LED_ms = pyb.millis()
+    clock = time.clock()
+
+
+init()
+
+### MAIN LOOP ###
 while(True):
 
     # go to deep sleep when not operation time
@@ -190,10 +220,7 @@ while(True):
         illumination.off(message="during voltage reading")
         # check voltage and save status, if battery too low -> sleep until sunrise
         vbat = vdiv_bat.read_voltage()
-        if (vbat!="NA" and vbat<vbat_minimum and not pyb.USB_VCP().isconnected()):
-            write_filevars(current_folder, picture_count, detection_count)
-            write_status(vbat,"Battery low - Sleeping",current_folder)
-            indicator_dsleep(solartime.time_until_sunrise()+30*60*1000,active_LED_interval_ms)
+        check_battery_sleep(vbat=vbat)
         write_status(vbat,"Script running - Normal operation",current_folder)
         # at night, turn ON selected illumination LEDs if always ON mode
         if(illumination.can_turn_on(is_night)):
@@ -210,18 +237,12 @@ while(True):
     if (exposure_mode!="auto" and (pyb.elapsed_millis(start_time_blending_ms) > expose_period_s*1000) and (not triggered or not frame_differencing_enabled)
     or (exposure_mode!="auto" and (pyb.elapsed_millis(start_time_blending_ms) > 2*expose_period_s*1000))):
         
-        # at night, turn ON selected illumination LEDs if not always OFF mode
-        if(illumination.can_turn_on(is_night)):
-            illumination.on(message="for exposure adjustment")
-
-        expose(exposure_mode,exposure_bias_day,exposure_bias_night,gain_bias,exposure_ms,gain_dB,is_night)
         #blend new frame only if frame differencing
         if (frame_differencing_enabled):
             print("Blending new frame, saving background image after",str(round(pyb.elapsed_millis(start_time_blending_ms)/1000)),"seconds")
             #take new picture
             picture_count += 1
-            img = sensor.snapshot()
-            picture_time = "-".join(map(str,time.localtime()[0:6]))
+            img, picture_time = take_picture(do_expose=True)
             # Blend in new frame. We're doing 256-alpha here because we want to
             # blend the new frame into the background. Not the background into the
             # new frame which would be just alpha. Blend replaces each pixel by
@@ -240,37 +261,14 @@ while(True):
         #reset blending time counter
         start_time_blending_ms = pyb.millis()
 
-        # after exposure adjustment, turn OFF selected illumination LEDs if not always ON mode
-        if (illumination.can_turn_off()):
-            illumination.off(message="to save power...")
-
     # TAKING PICTURE
-    #current image parameters
-    current_exposure=sensor.get_exposure_us()
-    current_gain=sensor.get_gain_db()
     #loop over exposure values
-    for b in exposure_values:
-        if (use_exposure_bracketing):
-            #fix the gain so image is stable
-            sensor.set_auto_gain(False, gain_db = current_gain)
-            print("Exposure bracketing bias:",b)
-            sensor.set_auto_exposure(False, \
-                exposure_us = int(current_exposure*b))
-            #wait for new exposure time to be applied
-            sensor.skip_frames(time = 2000)
-        # at night, turn ON selected illumination LEDs if not always OFF mode
-        if(illumination.can_turn_on(is_night)):
-            illumination.on(message="to take the picture")
+    for exposure_mult in exposure_values:
 
-        img = sensor.snapshot()
+        img, picture_time = take_picture(do_expose=False, exposure_mult=exposure_mult)
 
-        # after picture, turn OFF selected illumination LEDs if not always ON mode
-        if (illumination.can_turn_off()): 
-            illumination.off(message="to save power...")
-
-        #log time
-        picture_time = "-".join(map(str,time.localtime()[0:6]))
         #start cycling over ROIs
+        # rois_rects length==1 if use_roi==False
         for roi_temp in rois_rects:
             if (use_roi):
                 print("Extracting ROI:",roi_temp)
@@ -493,7 +491,7 @@ while(True):
             else: exposure_bias=exposure_bias_day
             # re-set exposure
             sensor.set_auto_exposure(False, \
-                exposure_us = int(current_exposure))
+                exposure_us = int(last_exposure))
             #wait for auto-adjustment
             sensor.skip_frames(time = 2000)
 
@@ -529,8 +527,5 @@ while(True):
 
         # (when light sleep is used) check voltage and save status, if battery too low -> sleep until sunrise
         vbat = vdiv_bat.read_voltage()
-        if (vbat!="NA" and vbat<vbat_minimum):
-            write_filevars(current_folder, picture_count, detection_count)
-            write_status(vbat,"Battery low - Sleeping",current_folder)
-            indicator_dsleep(solartime.time_until_sunrise()+30*60*1000,active_LED_interval_ms)
+        check_battery_sleep(vbat=vbat)
         write_status(vbat,"Delay loop - waking",current_folder)
