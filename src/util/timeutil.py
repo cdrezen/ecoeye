d8ac7a50cb579b1d2b05da89dee4733cf6a5e17a
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2025-07-03 16:52:16 +0200

    time.localtime() instead of RTC.datetime()

--- src/util/timeutil.py
+++ src/util/timeutil.py
@@ -17,107 +17,21 @@ SECONDS_IDX = 5
 SUNRISE_MS = cfg.SUNRISE_HOUR * MS_PER_HOUR + cfg.SUNRISE_MINUTE * MS_PER_MIN
 SUNSET_MS = cfg.SUNSET_HOUR * MS_PER_HOUR + cfg.SUNSET_MINUTE * MS_PER_MIN
 
-### deprecated ###
-class Suntime:
-
-    def __init__(self, op_t, sr_h, sr_m, ss_h, ss_m):
-        self.op_t = op_t
-        self.sr_h = sr_h
-        self.sr_m = sr_m
-        self.ss_h = ss_h
-        self.ss_m = ss_m
-
-    # ⚊⚊⚊⚊⚊ daytime check ⚊⚊⚊⚊⚊
-    # checks if its daytime or nightime
-    # --- Input arguments ---
-    # sunrise and sunset times
-    # --- Output variables ---
-    # daytime - boolean whever its day or not
-    def is_daytime(self):
-        # get current time in milliseconds
-        nowms = ((time.localtime()[3]*60+time.localtime()[4])*60+time.localtime()[5])*1000
-        # now is daytime
-        if ( nowms >= (self.sr_h*60+self.sr_m)*60*1000 and nowms < (self.ss_h*60+self.ss_m)*60*1000 ):
-            daytime = True
-        else:
-            daytime = False
-        return daytime
-
-    # ⚊⚊⚊⚊⚊ Time until sunrise ⚊⚊⚊⚊⚊
-    # calculates time until sunrise
-    # --- Input arguments ---
-    # sunrise and sunset times
-    # --- Output variables ---
-    # time_to_sunrise - in milliseconds
-    def time_until_sunrise(self):
-        # get current time in milliseconds
-        nowms = ((time.localtime()[3]*60+time.localtime()[4])*60+time.localtime()[5])*1000
-        daytime = self.is_daytime()
-        if (daytime):
-            time_to_sunrise = 0
-        else:
-            # get ms until sunrise
-            # calculation for before midnight
-            if(nowms >= (self.ss_h*60+self.ss_m)*60*1000 ):
-                time_to_sunrise = (24*60+self.sr_h*60+self.sr_m)*60*1000 - nowms
-            # calculation for after midnight
-            else:
-                time_to_sunrise = (self.sr_h*60+self.sr_m)*60*1000 - nowms
-        return time_to_sunrise
-    
-    # ⚊⚊⚊⚊⚊ Time until sunset ⚊⚊⚊⚊⚊
-    # calculate time until sunset
-    # --- Input arguments ---
-    # sunrise and sunset times
-    # --- Output variables ---
-    # time_to_sunset - in milliseconds
-    def time_until_sunset(self):
-        # get current time in milliseconds
-        nowms = ((time.localtime()[3]*60+time.localtime()[4])*60+time.localtime()[5])*1000
-        daytime = self.is_daytime()
-        if (daytime):
-            time_to_sunset = (self.ss_h*60+self.ss_m)*60*1000 - nowms
-        else:
-            time_to_sunset = 0
-        return time_to_sunset
-
-    # ⚊⚊⚊⚊⚊ operation time check ⚊⚊⚊⚊⚊
-    # check if operation time
-    # --- Input arguments ---
-    # sunrise and sunset times
-    # operationt time string
-    # --- Output variables ---
-    # operation_time_check - boolean
-    def is_operation_time(self):
-        #check time operation mode in day/night operation time modes
-        night_time_check = not self.is_daytime()
-        if(self.op_t=="day"):
-            operation_time_check = not night_time_check
-        if(self.op_t=="night"):
-            operation_time_check = night_time_check
-        if(self.op_t=="24h"):
-            operation_time_check = True
-        return operation_time_check
-###
-
-class Rtc:
-    def __init__(self):
-        # initialise RTC object
-        self.rtc = pyb.RTC()
-        # set rtc from user defined date and time only on power on
-        if (machine.reset_cause() != machine.DEEPSLEEP_RESET):
-            self.rtc.datetime(cfg.START_DATETIME)
-        
-    def datetime(self):
-        return self.rtc.datetime() # returns a tuple (year, month, day, weekday, hours, minutes, seconds, subseconds)
-
-    
-rtc = Rtc()
-
 clock = time.clock()
 
+def reset_rtc(datetime: tuple[int, int, int, int, int, int, int, int] = cfg.START_DATETIME):
+    pyb.RTC().datetime(datetime)
+
+### TODO: move to appropriate module or class
+# set rtc from user defined date and time only on power on
+if (machine.reset_cause() != machine.DEEPSLEEP_RESET):
+    reset_rtc()
+###
+
 def datetime():
-    return rtc.datetime()
+    # returns a tuple (year, month, day, weekday, hours, minutes, seconds, subseconds)
+    # https://forums.openmv.io/t/using-time-localtime-vs-rtc-datetime/11190/2
+    return time.localtime()
 
 def ms_since_midnight():
     """
